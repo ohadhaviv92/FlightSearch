8d1b09c42962414aea4b3b9ddd36fcde3f2036ba
ohad <ohadhaviv92@gmail.com>
2020-05-12 01:21:26 +0300

    airlines fixed

--- HW1/DBQuery.sql
+++ HW1/DBQuery.sql
@@ -1,27 +1,31 @@
---create table Airports_CS(
---AirportCode nvarchar primary key,
---airportname nvarchar(40),
---long int,
---lat int,
---airportcity nvarchar(30),
---airportCountry nvarchar(40),
+﻿--create table Airports_CS(
+--AirportCode nvarchar(40) primary key,
+--Airportname nvarchar(40),
+--Longitude nvarchar(15),
+--Latitudes nvarchar(15),
+--City nvarchar(30),
+--Country nvarchar(40),
 --)
 
+--INSERT INTO Airports_CS (AirportCode, Airportname, Longitude, Latitudes, City, Country) Values('LCG', 'A Coruña' ,-8.377222, 43.302222, 'A Coruña', 'Spain')
 
---create table Airlines_CS(
---AirlinesCode nvarchar primary key,
---Airportname nvarchar(40)
---)
+DROP TABLE Airlines_CS
 
---create table MyFlights_CS(
--- FlightPath nvarchar primary key,
--- AirportFrom  nvarchar, 
--- AirportTo nvarchar, 
--- DepTime nvarchar(40), 
--- ArriveTime nvarchar(40), 
--- Duration nvarchar(20),
--- Price float ,
--- LegsNumber int ,
--- FOREIGN KEY (AirportFrom) REFERENCES Airports_CS(AirportCode),
---  FOREIGN KEY (AirportTo) REFERENCES Airports_CS(AirportCode)
---)
+
+CREATE table Airlines_CS(
+AirlineCode nvarchar(40) primary key,
+AirlineName nvarchar(40)
+)
+
+create table MyFlights_CS(
+ FlightPath nvarchar(500) primary key,
+ AirportFrom  nvarchar(100), 
+ AirportTo nvarchar(100), 
+ DepTime nvarchar(40), 
+ ArriveTime nvarchar(40), 
+ Duration nvarchar(20),
+ Price float ,
+ LegsNumber int ,
+ FOREIGN KEY (AirportFrom) REFERENCES Airports_CS(AirportCode),
+  FOREIGN KEY (AirportTo) REFERENCES Airports_CS(AirportCode)
+)
