18fba7aedf711c6c1290c87f759f286236aae1e4
ohad <ohadhaviv92@gmail.com>
2020-05-11 22:45:30 +0300

    Create Airlines.cs

---
+++ HW1/Models/Airlines.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1.Models
+{
+    public class Airlines
+    {
+
+        int airlinesCode;
+        string airlinesName;
+
+        public Airlines(int airlinesCode, string airlinesName)
+        {
+            this.airlinesCode = airlinesCode;
+            this.airlinesName = airlinesName;
+        }
+
+        public int AirlinesCode { get => airlinesCode; set => airlinesCode = value; }
+        public string AirlinesName { get => airlinesName; set => airlinesName = value; }
+    }
+}
