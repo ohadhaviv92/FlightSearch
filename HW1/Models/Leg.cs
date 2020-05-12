245b1a67380e0bd3450af8eb5d175943dd4d3bc3
Dor Ben Zvi <55704483+dorbenzvi@users.noreply.github.com>
2020-05-12 12:59:45 +0300

    added leg class

--- HW1/Models/Leg.cs
+++ HW1/Models/Leg.cs
@@ -7,5 +7,15 @@ namespace HW1.Models
 {
     public class Leg
     {
+        string id;
+        string fullpathid;
+        int legnumber;
+        string flight_no;
+        string fromairport;
+        string toairport;
+        string airlinecode;
+        string DepTime;
+        string ArriveTime;
+        string Duration;
     }
 }
