29e9609b4478b2e256859f569a53f1cbadbedbd0
Dor Ben Zvi <55704483+dorbenzvi@users.noreply.github.com>
2020-05-22 14:18:57 +0300

    disocunt

---
+++ HW1/Models/Discount.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1.Models
+{
+    public class Discount
+    {
+        string airlineCode;
+        string AirportCodeFrom;
+        string AirportCodeTo;
+        DateTime DateFrom;
+        DateTime DateTo;
+        float discount;
+
+        public string AirlineCode { get => airlineCode; set => airlineCode = value; }
+        public string AirportCodeFrom1 { get => AirportCodeFrom; set => AirportCodeFrom = value; }
+        public string AirportCodeTo1 { get => AirportCodeTo; set => AirportCodeTo = value; }
+  
+      
+    }
+}
