2358f74b17b53522828e6b53ebecad2a63c7c6c5
ohad <ohadhaviv92@gmail.com>
2020-05-22 10:15:27 +0300

    Update Order.cs

--- HW1/Models/Order.cs
+++ HW1/Models/Order.cs
@@ -8,7 +8,7 @@ namespace HW1.Models
     public class Order
     {
 
-        int orderID;
+        
         string passengersNames;
         string email;
         
@@ -21,16 +21,16 @@ namespace HW1.Models
           
         }
 
-        public Order(int orderID, string passengersNames, string email, Flight flight)
+        public Order( string passengersNames, string email, Flight flight)
         {
-            this.orderID = orderID;
+            
             this.passengersNames = passengersNames;
             this.email = email;
             
             this.flight = flight;
         }
 
-        public int OrderID { get => orderID; set => orderID = value; }
+       
         public string PassengersNames { get => passengersNames; set => passengersNames = value; }
         public string Email { get => email; set => email = value; }
         
