47c97da961845cbecc1dcc96b2580afd7d20841c
ohad <ohadhaviv92@gmail.com>
2020-08-09 15:43:26 +0300

    /

---
+++ HW1/Models/Tour.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1.Models
+{
+    public class Tour
+    {
+        int id;
+        string agencyName;
+        string 
+
+    }
+}
