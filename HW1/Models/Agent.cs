ad2e84d1d59a0dbe67b437d4cceb168a92ba8c9c
Dor Ben Zvi <55704483+dorbenzvi@users.noreply.github.com>
2020-08-10 12:18:41 +0300

    h

---
+++ HW1/Models/Agent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HW1.Models
+{
+    public class Agent
+    {
+        string agentName;
+         string agentImage;
+         string password;
+
+        public Agent(string agentName, string agentImage, string password)
+        {
+            AgentName = agentName;
+            AgentImage = agentImage;
+            Password = password;
+        }
+
+        public string AgentName { get => agentName; set => agentName = value; }
+        public string AgentImage { get => agentImage; set => agentImage = value; }
+        public string Password { get => password; set => password = value; }
+    }
+}
